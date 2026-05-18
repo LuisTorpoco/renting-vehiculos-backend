2935694c150753de2df308261216480688694aa8
SOFTTEKPROJECTS\luis.torpoco <luis.torpoco@softtek.com>
2026-05-18 15:20:48 +0200

    creacion de interfaces VehicleRepository y ExtraRepository para el acceso a Oracle

--- backend/src/main/java/com/renting/backend/repositories/ExtraRepository.java
+++ backend/src/main/java/com/renting/backend/repositories/ExtraRepository.java
@@ -1,4 +1,13 @@
 package com.renting.backend.repositories;
 
-public class ExtraRepository {
+import com.renting.backend.entities.Extra;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+ // Repositorio para la gestión de datos de la entidad Extra en Oracle.
+@Repository
+public interface ExtraRepository extends JpaRepository<Extra, Long> {
+    //  solo los extras que estén activos evita traer extras que no se pueden alquilar.
+    List<Extra> findByIsActive(Integer isActive);
 }
