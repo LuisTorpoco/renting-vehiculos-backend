43b0dfe6e3efaf125aaa62a50773209afd502de0
SOFTTEKPROJECTS\alfredo.montaner <alfredo.montaner@softtek.com>
2026-05-19 10:32:05 +0200

    Creadas las entidades Customer e Income

--- backend/src/main/java/com/renting/backend/entities/Customer.java
+++ backend/src/main/java/com/renting/backend/entities/Customer.java
@@ -1,6 +1,73 @@
 package com.renting.backend.entities;
 
-public class Customer
-{
-    
+import com.renting.backend.enums.EmploymentStatus;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Table(name = "customer")
+@Getter
+@Setter
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy =
+            GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false,
+            unique = true,
+            length = 10)
+    private String nif;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(name = "first_surname",
+            nullable = false)
+    private String firstSurname;
+
+    @Column(name =
+            "second_surname")
+    private String secondSurname;
+
+    @Column(name = "nacionality")
+    private String nacionality;
+
+    @Column(nullable = false)
+    private LocalDate birthdate;
+
+    @Column(nullable = false)
+    private BigDecimal scoring;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name =
+            "employment_status")
+    private EmploymentStatus
+            employmentStatus;
+
+    @Column(nullable = false)
+    private String phone;
+
+    @Column(name = "non_payment")
+    private Boolean nonPayment;
+
+    @Column(name = "is_active")
+    private Boolean active;
+
+    @Column(name = "career_time")
+    private LocalDate careerTime;
+
+    @OneToMany(mappedBy =
+            "customer")
+    private List<Income> incomes;
+
+    @OneToMany(mappedBy =
+            "customer")
+    private List<Request> requests;
 }
--- backend/src/main/java/com/renting/backend/entities/Income.java
+++ backend/src/main/java/com/renting/backend/entities/Income.java
@@ -1,4 +1,39 @@
 package com.renting.backend.entities;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "income")
+@Getter
+@Setter
 public class Income {
+
+    @Id
+    @GeneratedValue(strategy =
+            GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name =
+            "pre_taxes")
+    private BigDecimal preTaxes;
+
+    @Column(name =
+            "post_taxes")
+    private BigDecimal postTaxes;
+
+    @Column(name =
+            "created_at")
+    private LocalDateTime
+            createdAt;
+
+    @ManyToOne(fetch =
+            FetchType.LAZY)
+    @JoinColumn(name =
+            "customer_id")
+    private Customer customer;
 }
