e407fec6bba760489f3b49df466c0e85bf69478c
SOFTTEKPROJECTS\luis.torpoco <luis.torpoco@softtek.com>
2026-05-19 16:19:51 +0200

    agregar FraudRiskRule para validar restriccion de edad minima legal

--- backend/src/main/java/com/renting/backend/services/scoring/rules/denial/FraudRiskRule.java
+++ backend/src/main/java/com/renting/backend/services/scoring/rules/denial/FraudRiskRule.java
@@ -1,4 +1,25 @@
 package com.renting.backend.services.scoring.rules.denial;
 
-public class FraudRiskRule {
+import com.renting.backend.services.scoring.context.ScoringContext;
+import com.renting.backend.services.scoring.rules.Rule;
+import org.springframework.stereotype.Component;
+import java.time.LocalDate;
+import java.time.Period;
+
+@Component
+public class FraudRiskRule implements Rule {
+
+    @Override
+    public boolean evaluate(ScoringContext context) {
+        LocalDate birthDate = context.getCustomer().getBirthdate();
+        if (birthDate == null) return false;
+
+        int age = Period.between(birthDate, LocalDate.now()).getYears();
+        return age >= 18;
+    }
+
+    @Override
+    public String getMessage() {
+        return "Solicitud denegada: Alerta de riesgo legal. El solicitante es menor de edad.";
+    }
 }
