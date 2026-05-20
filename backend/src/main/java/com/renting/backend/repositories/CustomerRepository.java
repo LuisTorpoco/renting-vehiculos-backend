03982955c0d550125860971223ab0eb34fd94a93
SOFTTEKPROJECTS\santiago.lopezj <santiago.lopezj@softtek.com>
2026-05-20 11:11:34 +0200

    Creado el Endpint de agregar ingresos a un cliente

--- backend/src/main/java/com/renting/backend/repositories/CustomerRepository.java
+++ backend/src/main/java/com/renting/backend/repositories/CustomerRepository.java
@@ -1,18 +1,35 @@
 package com.renting.backend.repositories;
 
 import com.renting.backend.entities.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.*;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
-@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
-    @Query("SELECT c FROM Customer c WHERE c.id = :id AND c.isActive = 1")
+    @Query("""
+        SELECT c
+        FROM Customer c
+        WHERE c.id = :id
+        AND c.isActive = 1
+    """)
     Optional<Customer> findActiveById(@Param("id") Long id);
 
-    @Query("SELECT COUNT(r) > 0 FROM Request r WHERE r.customer.id = :id AND r.isActive = 1")
+    @Query("""
+        SELECT c
+        FROM Customer c
+        WHERE c.isActive = 1
+    """)
+    Page<Customer> findAllActive(Pageable pageable);
+
+    @Query("""
+        SELECT COUNT(r) > 0
+        FROM Request r
+        WHERE r.customer.id = :id
+        AND r.isActive = 1
+    """)
     boolean hasActiveRequests(@Param("id") Long id);
 }
