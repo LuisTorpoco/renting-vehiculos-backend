fc3f2df06f7d2f63859fe005200d039913b68456
SOFTTEKPROJECTS\juand.castillo <juand.castillo@softtek.com>
2026-05-20 16:23:26 +0200

    DASHBOARD HECHO

---
+++ backend/src/main/java/com/renting/backend/services/DashboardService.java
@@ -0,0 +1,7 @@
+package com.renting.backend.services;
+
+import com.renting.backend.dtos.response.DashboardStatsResponse;
+
+public interface DashboardService {
+    DashboardStatsResponse getDashboardStats();
+}
