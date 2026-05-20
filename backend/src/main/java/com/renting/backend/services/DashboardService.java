8f6335937a334c86ff422fceee8a6ebc06eca103
SOFTTEKPROJECTS\juand.castillo <juand.castillo@softtek.com>
2026-05-20 16:34:02 +0200

    Revert "DASHBOARD HECHO"
    This reverts commit 446d26ef5f1d1d5caaccac25fa44821c97227e91.

--- backend/src/main/java/com/renting/backend/services/DashboardService.java
+++
@@ -1,7 +0,0 @@
-package com.renting.backend.services;
-
-import com.renting.backend.dtos.response.DashboardStatsResponse;
-
-public interface DashboardService {
-    DashboardStatsResponse getDashboardStats();
-}
